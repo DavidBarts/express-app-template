0c3eff9e53b0013305e1803d3c4e0142af0543bb
David Barts <david.w.barts@gmail.com>
2026-01-11 13:13:28 -0800

    Use await instead.

--- lib/renderSSX.ts
+++ lib/renderSSX.ts
@@ -2,13 +2,9 @@ import { renderComponent } from '@lumeland/ssx/jsx-runtime';
 import { Response } from 'express'
 
 export async function renderSSX(res: Response, it: JSX.Component | Promise<JSX.Component>): Promise<void> {
-    if (it instanceof Promise) {
-        it.then((resolved: JSX.Component) => renderSSX(res, resolved))
-          .catch((e: unknown) => { throw e instanceof Error ? e : new Error(e.toString()) });
-        return;
-    }
+    const component = it instanceof Promise ? await it : it
     try {
-        const page = await renderComponent(it)
+        const page = await renderComponent(component)
         res.set('Content-Type', 'text/html; charset=utf-8')
         res.send(page)
     } catch (e) {
